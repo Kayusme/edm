632e00ef37933584902a1a1cbff66844d62d9312
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-08-21 23:59:48 +0200

    [UPDATE] Assets.php

--- application/controllers/Assets.php
+++ application/controllers/Assets.php
@@ -5,8 +5,45 @@
  * Date: 21/08/2018
  * Time: 22:29 PM
  */
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Assets
+class Assets extends CI_Controller
 {
+    private $badBot = 0;
+    private $badIP;
+    public function index()
+    {
+        $this->badBot++;
+        if ($this->badBot == 3)
+            echo "Attention !...";
+        elseif ($this->badBot > 3)
+            $this->badIP = $_SERVER["REMOTE_ADDR"];
+        else
+            redirect(base_url());
+    }
 
+    public function css()
+    {
+        
+    }
+
+    public function js()
+    {
+        
+    }
+
+    public function images()
+    {
+        
+    }
+
+    public function fonts()
+    {
+        
+    }
+
+    public function media()
+    {
+        
+    }
 }
