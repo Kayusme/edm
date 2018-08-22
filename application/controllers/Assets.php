ab071f8073d256d6cff079d0ff0a3cb94de81541
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-08-22 18:18:48 +0200

    [UPDATE] ENcore dans ASSETS

--- application/controllers/Assets.php
+++ application/controllers/Assets.php
@@ -2,11 +2,27 @@
 /**
  * Created by PhpStorm.
  * User: ITOT
- * Date: 22/08/2018
- * Time: 01:25 AM
+ * Date: 21/08/2018
+ * Time: 22:29 PM
  */
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Assets
+class Assets extends CI_Controller
 {
+    public function index()
+    {
+        redirect(base_url());//TODO: import Font-Awesome from internet
+    }
 
+    public function statics(string $first = null, string $second = null,string $third = null)
+    {
+        $this->load->helper("file");
+        $mime = get_mime_by_extension($third);
+        if ($mime){
+            header("Content-type: ".$mime);
+            $this->load->view($first."/".$second."/".$third);
+        }else{
+            redirect(base_url());
+        }
+    }
 }
