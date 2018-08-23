243df35c79b3fab8a9875f80ce9aa07fd5d23df2
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-08-23 23:50:17 +0200

    [DEL] J'ai effacer imgFromDir d'ici

--- application/controllers/Eduquemoi.php
+++ application/controllers/Eduquemoi.php
@@ -5,7 +5,6 @@ class Eduquemoi extends CI_Controller {
 
     public function index()
     {
-        $this->load->helper('imgFromDir');
 //        $this->lang->load('fr','french');
         $data['title'] = 'eduque-moi';
         $this->load->view('eduquemoi/_global/header',$data);
