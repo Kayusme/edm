efb235e6cba273f002df36283905bb84d66cf5d7
Roland Beni <roland.beni1@gmail.com>
2018-09-17 10:50:21 +0200

    [UPDATE] session problem

--- application/controllers/Eleve.php
+++ application/controllers/Eleve.php
@@ -3,6 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Eleve  extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('session');
+    }
     public function index()
     {
 //        var_dump($this->session);die();
