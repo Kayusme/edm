608fa4a0ab8a8e684ff0eed066a3b012e4b06158
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-26 16:34:02 +0200

    [UPDATE] delete keywords

--- application/views/eleve/login.php
+++ application/views/eleve/login.php
@@ -4,8 +4,7 @@
 <title>Eleve | Edm</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Modern Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
-Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<meta name="keywords" content="" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
  <!-- Bootstrap Core CSS -->
 <link href="<?= BASE_URL() ?>assets/statics/eleve/css/bootstrap.min.css" rel='stylesheet' type='text/css' />
