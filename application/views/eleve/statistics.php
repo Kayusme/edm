8c0dcc32867c6bb9184b2ed94ccc30048d7420a8
ShekMak <majorsonshekinah@gmail.com>
2018-09-27 01:47:23 +0200

    Ajouter pour effectuer un test sur mes calculs.

--- application/views/eleve/statistics.php
+++ application/views/eleve/statistics.php
@@ -1,6 +1,10 @@
 <div id="page-wrapper">
   <div class="col-md-12 graphs">
     <h3 class="text-center">Evolutions statistique</h3class>
+            <div>
+                <h3>Test des statistiques</h3>
+                <?php echo $pourcentage." %"; ?>
+            </div>
            <div class="graph_box1">
 
               <div class="col-md-6 grid_2"><div class="grid_1">
