0ecde2eeac5d39bd7e37ef57ed77f97db3f0b3d7
Roland Beni <roland.beni1@gmail.com>
2018-09-27 17:37:07 +0200

    [UPDATE] cours design

--- application/views/eleve/cours.php
+++ application/views/eleve/cours.php
@@ -1,74 +1,23 @@
-<!--
-Author: W3layouts
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
-
-        <div id="page-wrapper">
-        <div class="graphs">
-	     <div class="widget_head">COURS</div>
+<div id="page-wrapper">
+    <div class="graphs">
+	    <div class="widget_head"><h1>COURS</h1></div>
+        <hr>
+            <div class="widget_1">
             <?php
                 foreach ($resultats as $resultat) {
             ?>
-		   <div class="widget_1">
-		 	 <div class="col-sm-3 widget_1_box">
-                <div class="tile-progress bg-info">
-                    <div class="content">
-                        <h4><i class="fa fa-dashboard icon-sm"></i> <?= strtoupper($resultat['nomCours'])?></h4>
-                        <div class="progress"><div class="progress-bar inviewport animated visible slideInLeft" style="width: 40%;"></div></div>
-                        <span><?= ucfirst($resultat['descriptionCours'])?></span>
+                <div class="col-sm-3 widget_1_box">
+                    <div class="tile-progress bg-info">
+                        <div class="content">
+                            <h4><i class="fa fa-dashboard icon-sm"></i> <?= strtoupper($resultat['nomCours'])?></h4><br>
+                            <!-- <div class="progress"><div class="progress-bar inviewport animated visible slideInLeft" style="width: 40%;"></div></div> -->
+                            <span><?= ucfirst($resultat['descriptionCours'])?></span>
+                        </div>
                     </div>
-                </div>
-             </div>
-              <div class="clearfix"> </div>
-		   </div>
+                </div> 
             <?php }?>
-
-		   <div class="widget_1">
-             <div class="col-sm-3 widget_1_box">
-                <div class="tile-progress bg-info">
-                    <div class="content">
-                        <h4><i class="fa fa-dashboard icon-sm"></i> EDUCATION A LA VIE</h4>
-                        <div class="progress"><div class="progress-bar inviewport animated visible slideInLeft" style="width: 40%;"></div></div>
-                        <span></span>
-                    </div>
-                </div>
-             </div>
-             <div class="col-sm-3 widget_1_box">
-                <div class="tile-progress bg-success">
-                    <div class="content">
-                        <h4><i class="fa fa-dashboard icon-sm"></i> EDUQUATION CIVIQUE</h4>
-                        <div class="progress"><div class="progress-bar inviewport animated visible slideInLeft" style="width: 40%;"></div></div>
-                        <span></span>
-                    </div>
-                </div>
-             </div>
-             <div class="col-sm-3 widget_1_box">
-               <div class="tile-progress bg-danger">
-                    <div class="content">
-                        <h4><i class="fa fa-dashboard icon-sm"></i> GEOGRAPHIE</h4>
-                        <div class="progress"><div class="progress-bar inviewport animated visible slideInLeft" style="width: 40%;"></div></div>
-                        <span></span>
-                    </div>
-                </div>
-             </div>
-             <div class="col-sm-3 widget_1_box">
-                <div class="tile-progress bg-secondary">
-                    <div class="content">
-                        <h4><i class="fa fa-dashboard icon-sm"></i> HISTOIRE</h4>
-                        <div class="progress"><div class="progress-bar inviewport animated visible slideInLeft" style="width: 40%;"></div></div>
-                        <span></span>
-                    </div>
-                </div>
-              </div>
-              <div class="clearfix"> </div>
-           </div>
-		   	 <div class="clearfix"> </div>
-		   </div>
-	  </div>
-      </div>
-      <!-- /#page-wrapper -->
-   </div>
-    <!-- /#wrapper -->
-
+                <div class="clearfix"> </div>
+            </div>
+		</div>
+	</div>
+</div>
