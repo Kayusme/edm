c9bf4e494db56fc3aaf7367911800d2824032706
ShekMak <majorsonshekinah@gmail.com>
2018-09-28 22:25:58 +0200

    J'ai ajouté une méthode qui permet qu'on reccupère la côte de l'étudiant en fonction du cours dispensé et de la période et l'autre méthode permet maintenant toutes les côtes de la promotion

--- application/models/Cote_model.php
+++ application/models/Cote_model.php
@@ -3,11 +3,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Cote_model extends CI_Model
 {
-    public function selectCote($id_dispenser, $id_periode)
+
+    public function selectCotes($id_dispenser, $id_periode)
     {
         $this->db->select('cote');
         $this->db->where('idDispenser', $id_dispenser);
         $this->db->where('idPeriode', $id_periode);
+        $this->db->from('cote');
+        
+        $lignes = $this->db->get();
+        
+    }
+
+    public function selectCote($id_dispenser, $id_periode, $id_eleve)
+    {
+        $this->db->select('cote');
+        $this->db->where('idDispenser', $id_dispenser);
+        $this->db->where('idPeriode', $id_periode);
+        $this->db->where('idEleve', $id_eleve);
         $this->db->from('cote');
         
         $lignes = $this->db->get();
