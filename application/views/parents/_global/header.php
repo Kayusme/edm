ac7d226025ebe591fe0d0fe6ff202509cfe512fd
Roland Beni <roland.beni1@gmail.com>
2018-09-28 22:52:40 +0200

    [UPDATE] design new look

--- application/views/parents/_global/header.php
+++ application/views/parents/_global/header.php
@@ -1,11 +1,13 @@
-<!DOCTYPE HTML>
-<html>
+<?= doctype("html5")."\n"?>
+<html lang="fr">
 <head>
-    <title>Creative Ui Kit Flat Bootstarp Responsive Website Template | Home :: w3layouts</title>
-    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-    <?php
-    header("Cache-Control: no-cache");
-    header("Pragma: no-cache");
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Start your development with a Design System for Bootstrap 4.">
+  <meta name="author" content="Creative Tim">
+  <title>Eduquemoi :: Parents</title>
+  <!-- //Meta-Tags -->
+  <?php
     $meta = array(
         array(
             'name' => 'robots',
@@ -17,7 +19,7 @@
         ),
         array(
             'name' => 'keywords',
-            'content' => 'eduquemoi, katanga, eduque, eduque-moi, lubumbashi, epsp, eduque-epsp, rdc, education, enseignement, national, p-breakers'
+            'content' => 'katanga, eduquemoi, education, lubumbashi'
         ),
         array(
             'name' => 'author',
@@ -32,50 +34,20 @@
             'content'=>'width=device-width, initial-scale=1'
         )
     );
-//    <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+
     echo meta($meta).
         '<!-- Icon-CSS -->'."\n".
-        link_tag("assets/statics/eduquemoi/images/icone.png",'icon').
-        '<!-- Bootstrap-CSS -->'."\n".
-        link_tag("assets/statics/parents/css/bootstrap.css").
-        '<!-- Calender-CSS -->'."\n".
-        link_tag("assets/statics/parents/css/style.css").
-        link_tag("assets/statics/parents/css/clndr.css").
-        '<!-- Other-CSS -->'."\n".
-        link_tag("assets/statics/parents/css/theme.css").
-        '<!-- Index-Page-CSS -->'."\n".
-        link_tag("assets/statics/parents/css/audio.css").
-        link_tag("assets/statics/parents/css/form.css").
-        '<!-- Headings-font -->'."\n".
-        link_tag("//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800").
-        '<!-- Body-font -->'."\n".
-        link_tag("//fonts.googleapis.com/css?family=Lato:300,400,700").
-        '<!--//web-fonts-->'."\n".'<!--//fonts-->'."\n".'<!-- js -->'."\n";
-    ?>
-    <script src="<?=base_url("assets/statics/parents/js/jquery.min.js")?>"></script>
-    <script src="<?=base_url("assets/statics/parents/js/jquery.circlechart.js")?>"></script>
-    <script src="<?=base_url("assets/statics/parents/js/underscore-min.js")?>"></script>
-    <script src= "<?=base_url("assets/statics/parents/js/moment-2.2.1.js")?>"></script>
-    <script src="<?=base_url("assets/statics/parents/js/clndr.js")?>"></script>
-    <script src="<?=base_url("assets/statics/parents/js/site.js")?>"></script>
-    <!----End Calender -------->
-
-    <!----start audio ------->
-    <script type="text/javascript" src="<?=base_url("assets/statics/parents/js/mediaelement-and-player.min.js")?>"></script>
-    <!-- --End audio ---- -->
-    <script type="text/javascript" src="<?=base_url("assets/statics/parents/js/jquery.jscrollpane.min.js")?>"></script>
-    <script type="text/javascript" id="sourcecode">
-        $(function()
-        {
-            $('.scroll-pane').jScrollPane();
-        });
-    </script>
-
+        link_tag("assets/statics/parents/images/icone.png",'icon').
+        '<!-- Argon-CSS -->'."\n".
+        link_tag("assets/statics/parents/css/argon.css?v=1.0.1").
+        '<!-- Font-awesome-CSS -->'."\n".
+       '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>'."\n".
+        '<!-- Docs-CSS -->'."\n".
+        link_tag("assets/statics/parents/css/docs.min.css.css").
+        link_tag("assets/statics/parents/vendor/nucleo/css/nucleo.css").
+        '<!--//web-fonts-->'."\n".
+        link_tag("hhttps://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700")
+  ?>
 </head>
-<body>
-<div class="header">
-    <div class="container">
-        <div class="logo">
-            <h1><a href="<?=site_url("parents/index")?>">Eduque-Moi</a></h1>
-        </div>
 
+<body>
