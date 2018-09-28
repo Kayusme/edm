fa5130b2362db7777151dcbac3a6fa3d04dbcc89
ShekMak <majorsonshekinah@gmail.com>
2018-09-29 00:38:55 +0200

    J'ai implenté la méthode qui me permet de reccuperer la dernière classe de l'élève grâce à son id

--- application/models/Parcourt_model.php
+++ application/models/Parcourt_model.php
@@ -3,3 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Parcourt_model extends CI_Model
 {
+    public function selectClasseEleve($id_eleve)
+    {
+        $this->db->select('idClasse');
+        $this->db->where('idEleve', $id_eleve);
+        $this->db->order_by('idParcourt', 'DESC');
+        $this->db->limit(1);
+        $this->db->from('parcourt');
+        
+        $lignes = $this->db->get();
+        
+        foreach ($lignes->result() as $ligne) {
+            return $ligne->idClasse;
+        }
+    }
+}
