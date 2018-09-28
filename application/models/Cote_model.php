a11ba26364d6ed11ba33e3c48e93bd9194ef1b31
ShekMak <majorsonshekinah@gmail.com>
2018-09-28 21:53:07 +0200

    Implementation de la méthode qui affiche les cotes selon le cours dispensé et la période en cours

--- application/models/Cote_model.php
+++ application/models/Cote_model.php
@@ -3,3 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Cote_model extends CI_Model
 {
+    public function selectCote($id_dispenser, $id_periode)
+    {
+        $this->db->select('cote');
+        $this->db->where('idDispenser', $id_dispenser);
+        $this->db->where('idPeriode', $id_periode);
+        $this->db->from('cote');
+        
+        $lignes = $this->db->get();
+        
+        foreach ($lignes->result() as $ligne) {
+            return $ligne->cote;
+        }
+    }
+
+}
