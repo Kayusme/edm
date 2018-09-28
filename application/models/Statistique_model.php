fe7f05e7fbd38e6949f79c99abe3ddb166376ba4
ShekMak <majorsonshekinah@gmail.com>
2018-09-28 22:14:23 +0200

    J'ai supprimé toutes les méthodes qui reccupérer les valeurs des cotes obtenues et des cotes maximales du cours en laissant simplement la méthode qui permet d'obtenir le pourcentage selon le besoin

--- application/models/Statistique_model.php
+++ application/models/Statistique_model.php
@@ -3,55 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Statistique_model extends CI_Model
 {
-    public $id_matiere;
-    public $id_cours_dispense;
-    public $id_periode;
 
-    public function cours_dispense($id_matiere)
+    public function pourcentage($minimum, $maximum)
     {
-        $this->db->select('id');
-        $this->db->where('idMatiere',$id_matiere);
-        $this->db->from('dispenser');
-
-        $lignes = $this->db->get();
-        
-        foreach ($lignes->result() as $ligne) {
-            return $ligne->id;
-        }
-
-    }
-
-    public function valeur_maximale($id_matiere)
-    {
-        $this->db->select('maxi');
-        $this->db->where('idMatiere',$id_matiere);
-        $this->db->from('dispenser');
-        
-        $lignes = $this->db->get();
-
-        foreach ($lignes->result() as $ligne) {
-            return $ligne->maxi;
-        }
-    }
-
-    public function valeur_minimale($id_cours_dispense)
-    {
-        $this->db->select('cote');
-        $this->db->where('idDispenser', $id_cours_dispense);
-        $this->db->from('cote');
-        
-        $lignes = $this->db->get();
-        
-        foreach ($lignes->result() as $ligne) {
-            return $ligne->cote;
-        }
-
-    }
-
-    public function pourcentage($id_matiere)
-    {
-        $id_cours_dispense = $this->cours_dispense($id_matiere);
-        $valeur_sans_pourcentage = $this->valeur_minimale($id_cours_dispense) / $this->valeur_maximale($id_matiere);
+        $valeur_sans_pourcentage = $minimum / $maximum;
         $valeur_avec_pourcentage = $valeur_sans_pourcentage * 100;
 
         return $valeur_avec_pourcentage;
