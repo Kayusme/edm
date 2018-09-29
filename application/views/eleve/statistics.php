65b44e98dcf8fc5f4f5a5606b29f7bcbc61864c8
Roland Beni <roland.beni1@gmail.com>
2018-09-29 16:50:58 +0200

    [NEW] design->statistic

--- application/views/eleve/statistics.php
+++ application/views/eleve/statistics.php
@@ -1,9 +1,7 @@
 <div id="page-wrapper">
   <div class="col-md-12 graphs">
-
     <h3 class="text-center">Evolutions statistique</h3>
            <div class="graph_box1">
-
               <div class="col-md-12 grid_2">
                 <div class="grid_1">
 <!--                <h3>Bar</h3>-->
@@ -16,8 +14,87 @@
                   <canvas id="pie" height="500" width="1000" style="width: 1000px; height: 500px;"></canvas>
                 </div>
               </div>
+              <div class="col-md-6 col_5">
+
+				<div id="chart"></div>
+	       		<div id="slider"></div>
+				<script>
+
+			var seriesData = [ [], [], [], [], [] ];
+			var random = new Rickshaw.Fixtures.RandomData(50);
+
+			for (var i = 0; i < 75; i++) {
+				random.addData(seriesData);
+			}
+
+			var graph = new Rickshaw.Graph( {
+				element: document.getElementById("chart"),
+				renderer: 'multi',
+				
+				dotSize: 5,
+				series: [
+					{
+						name: 'temperature',
+						data: seriesData.shift(),
+						color: '#AFE9FF',
+						renderer: 'stack'
+					}, {
+						name: 'heat index',
+						data: seriesData.shift(),
+						color: '#FFCAC0',
+						renderer: 'stack'
+					}, {
+						name: 'dewpoint',
+						data: seriesData.shift(),
+						color: '#555',
+						renderer: 'scatterplot'
+					}, {
+						name: 'pop',
+						data: seriesData.shift().map(function(d) { return { x: d.x, y: d.y / 4 } }),
+						color: '#555',
+						renderer: 'bar'
+					}, {
+						name: 'humidity',
+						data: seriesData.shift().map(function(d) { return { x: d.x, y: d.y * 1.5 } }),
+						renderer: 'line',
+						color: '#ef553a'
+					}
+				]
+			} );
+
+
+			graph.render();
+
+			var detail = new Rickshaw.Graph.HoverDetail({
+				graph: graph
+			});
+				</script>
+		</div>
+	      <!-- map -->
+<link href="<?=base_url("assets/statics/eleve/css/jqvmap.css")?>" rel='stylesheet' type='text/css' />
+<script src="<?=base_url("assets/statics/eleve/js/jquery.vmap.js")?>"></script>
+<script src="<?=base_url("assets/statics/eleve/js/jquery.vmap.sampledata.js")?>" type="text/javascript"></script>
+<script src="<?=base_url("assets/statics/eleve/js/jquery.vmap.world.js")?>" type="text/javascript"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function() {
+		jQuery('#vmap').vectorMap({
+		    map: 'world_en',
+		    backgroundColor: '#333333',
+		    color: '#ffffff',
+		    hoverOpacity: 0.7,
+		    selectedColor: '#666666',
+		    enableZoom: true,
+		    showTooltip: true,
+		    values: sample_data,
+		    scaleColors: ['#C8EEFF', '#006491'],
+		    normalizeFunction: 'polynomial'
+		});
+	});
+</script>
+<!-- //map -->
               <div class="clearfix"> </div>
             </div>
+            
             <script>
     var barChartData = {
       labels : [<?=$cours?>],
