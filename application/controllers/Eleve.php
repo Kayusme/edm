5d76c57f6a5b8de1691d5c105518b83bf4f0ab0b
ShekMak <majorsonshekinah@gmail.com>
2018-09-29 12:16:24 +0200

    J'ai ajouté la partie qui reccupère les points de la première période

--- application/controllers/Eleve.php
+++ application/controllers/Eleve.php
@@ -112,6 +112,35 @@ class Eleve  extends CI_Controller
         //Tableaux contenant tous les id des matieres
         $data["matieres"] = $this->dispenser_model->selectIdMatiereDispenserByClasse($data["classe"]);
 
+        //Début de la reccupération des points de la première période
+
+        $i = 0;
+
+        foreach ($data["id_cours_dispenses"] as $id_cours_dispense) {
+            $cote[$i] = $this->cote_model->selectCote($id_cours_dispense,1,$data["eleve"]);
+            $i++;
+        }
+
+        $i = 0;
+
+        foreach ($data["matieres"] as $matiere) {
+            $max[$i] = $this->dispenser_model->selectMaximum($data["classe"],$matiere);
+            $i++;
+        }
+
+        for ($i=0; $i < count($max); $i++) { 
+
+            if ($i != count($max) - 1) {
+                //echo $cote[$i]." / ".$max[$i]." * 100 = ";
+                $data["periode1"] = $this->statistique_model->pourcentage($cote[$i], $max[$i]).", ";
+            } else {
+                $data["periode1"] = $this->statistique_model->pourcentage($cote[$i], $max[$i]);
+            }         
+
+        }
+
+        //Fin de la reccupération des points de la première période
+
         $this->load->view("eleve/_global/header",$data);
         $this->load->view("eleve/_global/nav");
         $this->load->view("eleve/statistics",$data);
