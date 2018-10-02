a696f72934cec38a78c5fdcb795d30d5f8a4480a
ShekMak <majorsonshekinah@gmail.com>
2018-10-02 22:40:48 +0200

    J'ai ajouté la méthode qui me permettra d'implementer le verificateur de synthaxe

---
+++ application/controllers/Sms.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ROBEN
+ * Date: 22/08/2017
+ * Time: 22:25
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Sms extends CI_Controller
+{
+
+    public function synthaxe_requete($requete)
+    {
+        
+    }
+
+}
