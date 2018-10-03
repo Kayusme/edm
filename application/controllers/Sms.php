8782fa949b0db671ed41d5ef1c9e54a331e836ce
ShekMak <majorsonshekinah@gmail.com>
2018-10-03 12:05:38 +0200

    Travail en cours

--- application/controllers/Sms.php
+++ application/controllers/Sms.php
@@ -13,7 +13,23 @@ class Sms extends CI_Controller
 
     public function synthaxe_requete($requete)
     {
-        
+        $message = explode(" ",$requete);
+
+        if (count($message) == 2) {
+            $matricule = $message[0];
+            $periode = $message[1];
+        }elseif (count($message) == 3) {
+            $matricule = $message[0];
+            $horaire = $message[1];
+            $jour = $message[2];
+        }
+
+        print_r($message);
+    }
+
+    public function index()
+    {
+        $this->synthaxe_requete("12dj34 1P");
     }
 
 }
