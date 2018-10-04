d20268e3c191d41eff9ebd9d0b48ffa51585b1fd
ShekMak <majorsonshekinah@gmail.com>
2018-10-04 10:09:30 +0200

    Je suis en pleine modification de la méthode pour obtenir un résultat satsfaisant donc je check déjà la synthaxe mais je n'ai pas pu encore bien tester cela

--- application/controllers/Sms.php
+++ application/controllers/Sms.php
@@ -11,7 +11,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Sms extends CI_Controller
 {
 
-    public function synthaxe_requete($requete)
+    public function synthaxe_requete($requete, $numero_telephone)
     {
         $message = explode(" ",$requete);
 
@@ -22,14 +22,34 @@ class Sms extends CI_Controller
             $matricule = $message[0];
             $horaire = $message[1];
             $jour = $message[2];
+        }else {
+            $AUTH_ID = 'MAMMU2MDRMNGE1Y2Y0YT';
+            $AUTH_TOKEN = 'MjA1MzBkODE5ZTljZDI0NzViMTk4YTkwYjE2ZmE0';
+            $src = '+243991888702';
+            $dst = $numero_telephone;
+            $text = 'Erreur dans la syntaxe. Veuillez rééssayer ou dirigez-vous vers https://eduquemoi.org/parent/bulletin.html';
+            $url = 'https://api.plivo.com/v1/Account/'.$AUTH_ID.'/Message/';
+            $data = array("src" => "$src", "dst" => "$dst", "text" => "$text");
+            $data_string = json_encode($data);
+            $ch=curl_init($url);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+            curl_setopt($ch, CURLOPT_HEADER, true);
+            curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
+            curl_setopt($ch, CURLOPT_USERPWD, $AUTH_ID . ":" . $AUTH_TOKEN);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+            $response = curl_exec( $ch );
+            curl_close($ch);
+            
+            return [];
         }
-
-        print_r($message);
+        return [];
     }
 
     public function index()
     {
-        $this->synthaxe_requete("12dj34 1P");
+        //$this->synthaxe_requete("12dj34 1P");
+        $this->load->library("../controllers/eleve");
     }
 
 }
