5556cf488d1f568e78d5e9999fb84225b14da331
ShekMak <majorsonshekinah@gmail.com>
2018-10-05 11:23:41 +0200

    La methode qui reccupère les résultats en forme de pourcentage

--- application/controllers/Sms.php
+++ application/controllers/Sms.php
@@ -11,6 +11,194 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Sms extends CI_Controller
 {
 
+    public function statistic_out()
+    {
+        
+        $this->load->model('eleve_model');
+        $this->load->model('statistique_model');
+        $this->load->model('cote_model');
+        $this->load->model('dispenser_model');
+        $this->load->model('parcourt_model');
+        $this->load->model('matiere_model');   
+
+        //La valeur de l'id de l'élève qui vient de la session sera placée là. 
+        $data["eleve"] = 1;
+
+        //Reccupère la dernière classe de l'élève
+        $data["classe"] = $this->parcourt_model->selectClasseEleve($data["eleve"]);
+
+        //Tableaux contenant tous les id de la table dispenser
+        $data["id_cours_dispenses"] = $this->dispenser_model->selectIdDispenser($data["classe"]);
+        
+        //Tableaux contenant tous les id des matieres
+        $data["matieres"] = $this->dispenser_model->selectIdMatiereDispenserByClasse($data["classe"]);
+
+        //Liste des matières
+        for ($i=0; $i < count($data["matieres"]); $i++) { 
+
+            if ($i != count($data["matieres"]) - 1) {
+                $data["cours"] = '"'.$this->matiere_model->selectNomMatiere($data["matieres"][$i]).'",';
+            } else {
+                $data["cours"] .= '"'.$this->matiere_model->selectNomMatiere($data["matieres"][$i]).'"';
+            }         
+
+        }
+
+        //Début de la reccupération des points de la période 1
+
+        $i = 0;
+
+        foreach ($data["id_cours_dispenses"] as $id_cours_dispense) {
+            $cote[$i] = $this->cote_model->selectCote1($id_cours_dispense,1,$data["eleve"]);
+            $i++;
+        }
+
+        $i = 0;
+
+        foreach ($data["matieres"] as $matiere) {
+            $max[$i] = $this->dispenser_model->selectMaximum($data["classe"],$matiere);
+            $i++;
+        }
+
+        for ($i=0; $i < count($max); $i++) { 
+
+            if ($i != count($max) - 1) {
+                $data["periode1"] = $this->statistique_model->pourcentage($cote[$i], $max[$i]).", ";
+                $resulat[$i] =  $this->statistique_model->pourcentage($cote[$i], $max[$i]);
+            } else {
+                $data["periode1"] .= $this->statistique_model->pourcentage($cote[$i], $max[$i]);
+                $resulat[$i] =  $this->statistique_model->pourcentage($cote[$i], $max[$i]);
+            }         
+
+        }
+        $data["resultats"] = $resulat;
+
+        //Fin de la reccupération des points de la période 1
+
+        //Début de la reccupération des points de la période2
+
+        $i = 0;
+
+        foreach ($data["id_cours_dispenses"] as $id_cours_dispense) {
+            $cote[$i] = $this->cote_model->selectCote($id_cours_dispense,2,$data["eleve"]);
+            $i++;
+        }
+
+        for ($i=0; $i < count($max); $i++) { 
+
+            if ($i != count($max) - 1) {
+                //echo $cote[$i]." / ".$max[$i]." * 100 = ";
+                $data["periode2"] = $this->statistique_model->pourcentage($cote[$i], $max[$i]).", ";
+            } else {
+                $data["periode2"] .= $this->statistique_model->pourcentage($cote[$i], $max[$i]);
+            }         
+
+        }
+
+        //Fin de la reccupération des points de la période 2
+
+        //Début de la reccupération des points de la période 3
+
+        $i = 0;
+
+        foreach ($data["id_cours_dispenses"] as $id_cours_dispense) {
+            $cote[$i] = $this->cote_model->selectCote($id_cours_dispense,3,$data["eleve"]);
+            $i++;
+        }
+
+        $i = 0;
+
+        foreach ($data["matieres"] as $matiere) {
+            $max[$i] = $this->dispenser_model->selectMaximum($data["classe"],$matiere);
+            $i++;
+        }
+
+        for ($i=0; $i < count($max); $i++) { 
+
+            if ($i != count($max) - 1) {
+                $data["periode3"] = $this->statistique_model->pourcentage($cote[$i], $max[$i]).", ";
+            } else {
+                $data["periode3"] .= $this->statistique_model->pourcentage($cote[$i], $max[$i]);
+            }         
+
+        }
+
+        //Fin de la reccupération des points de la période 3
+
+        //Début de la reccupération des points de la période4
+
+        $i = 0;
+
+        foreach ($data["id_cours_dispenses"] as $id_cours_dispense) {
+            $cote[$i] = $this->cote_model->selectCote($id_cours_dispense,4,$data["eleve"]);
+            $i++;
+        }
+
+        for ($i=0; $i < count($max); $i++) { 
+
+            if ($i != count($max) - 1) {
+                $data["periode4"] = $this->statistique_model->pourcentage($cote[$i], $max[$i]).", ";
+            } else {
+                $data["periode4"] .= $this->statistique_model->pourcentage($cote[$i], $max[$i]);
+            }         
+
+        }
+
+        //Fin de la reccupération des points de la période 4
+
+        //Début de la reccupération des points de l'examen 1
+
+        $i = 0;
+
+        foreach ($data["id_cours_dispenses"] as $id_cours_dispense) {
+            $cote[$i] = $this->cote_model->selectCote($id_cours_dispense,5,$data["eleve"]);
+            $i++;
+        }
+
+        $i = 0;
+
+        foreach ($data["matieres"] as $matiere) {
+            $max[$i] = $this->dispenser_model->selectMaximum($data["classe"],$matiere) * 2;
+            $i++;
+        }
+
+        for ($i=0; $i < count($max); $i++) { 
+
+            if ($i != count($max) - 1) {
+                $data["examen1"] = $this->statistique_model->pourcentage($cote[$i], $max[$i]).", ";
+            } else {
+                $data["examen1"] .= $this->statistique_model->pourcentage($cote[$i], $max[$i]);
+            }         
+
+        }
+
+        //Fin de la reccupération des points de l'examen 1
+
+        //Début de la reccupération des points de l'examen 2
+
+        $i = 0;
+
+        foreach ($data["id_cours_dispenses"] as $id_cours_dispense) {
+            $cote[$i] = $this->cote_model->selectCote($id_cours_dispense,6,$data["eleve"]);
+            $i++;
+        }
+
+        for ($i=0; $i < count($max); $i++) { 
+
+            if ($i != count($max) - 1) {
+                //echo $cote[$i]." / ".$max[$i]." * 100 = ";
+                $data["examen2"] = $this->statistique_model->pourcentage($cote[$i], $max[$i]).", ";
+            } else {
+                $data["examen2"] .= $this->statistique_model->pourcentage($cote[$i], $max[$i]);
+            }         
+
+        }
+
+        //Fin de la reccupération des points de l'examen 2
+        return $data;
+    }
+
+
     public function synthaxe_requete($requete, $numero_telephone)
     {
         $message = explode(" ",$requete);
@@ -60,6 +248,7 @@ class Sms extends CI_Controller
     {
         //$this->synthaxe_requete("12dj34 1P");
         //$this->load->library("../controllers/eleve");
+        var_dump($this->statistic_out());die();
     }
 
 }
