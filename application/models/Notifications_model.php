959aa6ddf03e8dca1962f8005bd74a28c499840a
savalone47 <savalonescott47@gmail.com>
2018-10-14 23:05:15 +0200

    [number of notifications]

--- application/models/Notifications_model.php
+++ application/models/Notifications_model.php
@@ -13,20 +13,25 @@ class Notifications_model extends CI_Model
     }
     public function unreadNotifications()
     {
+        
         $this->db->select('*');
         $this->db->from('notifications');
         $this->db->where('read_n',0);
         $this->db->order_by('date','DESC');
-		
+
+        
 		return $this->db->get()->result_array();
     }
     public function readNotification($id)
     {
-        $this->db->from('notifications');
-        $this->db->update('read_n',1);
-        $this->db->where('id',$id);
-		
-		return $this->db->get()->result_array();
-    }
+        if($id != 0){
 
+            $this->db->from('notifications');
+            $this->db->update('read_n',1);
+            $this->db->where('id',$id);
+        }
+        
+        return $this->db->get()->result_array();
+        
+    }
 }
