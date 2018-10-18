e643078d5c9a1b5fd1e6f885e0ff116ddaf802bc
elsa.m <manangelsa07@gmail.com>
2018-10-18 20:58:40 +0200

    [UPDATE] debut de la mise en place du formulaire d'admission

--- application/views/ecole/admissions.php
+++ application/views/ecole/admissions.php
@@ -31,103 +31,42 @@
 			    <div class="clearfix"> </div>
 					</div>
                     <div class="col-md-5 admi-main-right">
-                        <h4>Annonces</h4>
-                        <div class="admi-right">
-                            <div class="admi-right-left">
-                                <h5>1</h5>
-                            </div>
-                            <div class="admi-right-right">
-                                <h6>Anniversaire de l'école.</h6>
-                                <p>Nov  29,2013</p>
-                            </div>
-                            <div class="clearfix"></div>
-                        </div>
-                        <div class="admi-right">
-                            <div class="admi-right-left">
-                                <h5>2</h5>
-                            </div>
-                            <div class="admi-right-right">
-                                <h6>Rentrée scolaire</h6>
-                                <p>oct  23,2013</p>
-                            </div>
-                            <div class="clearfix"></div>
-                        </div>
-                        <div class="admi-right">
-                            <div class="admi-right-left">
-                                <h5>3</h5>
-                            </div>
-                            <div class="admi-right-right">
-                                <h6>Debut des Vacances de Noël</h6>
-                                <p>dec  18,2013</p>
-                            </div>
-                            <div class="clearfix"></div>
-                        </div>
-                        <div class="admi-right">
-                            <div class="admi-right-left">
-                                <h5>4</h5>
-                            </div>
-                            <div class="admi-right-right">
-                                <h6>Debut des Vacances de Pâques</h6>
-                                <p>mar  13,2013</p>
-                            </div>
-                            <div class="clearfix"></div>
-                        </div>
-                        <div class="admi-right">
-                            <div class="admi-right-left">
-                                <h5>5</h5>
-                            </div>
-                            <div class="admi-right-right">
-                                <h6>Fête de Nouvels an avec les enseignants</h6>
-                                <p>jan  3,2013</p>
-                            </div>
-                            <div class="clearfix"></div>
-                        </div>
+                        <h4>Formulaire d'admission</h4><br>
+                        <form action="" method="POST">
+                        	<input type="text" name="nom" placeholder="Nom : "><br><br>
+                        	<input type="text" name="post_nom" placeholder="Post-om : "><br><br>
+                        	<input type="text" name="prenom" placeholder="Prenom : "><br><br>
+                        	Genre : F <input type="radio" name="sex">
+                        	M <input type="radio" name="sex"><br><br>
+                        	Date de naissance : 
+                        	JJ <select>
+                        		<?php for($i = 1; $i <= 31; $i++) {?>
+                        		<option><?=$i ?></option>
+                        		<?php } ?>
+                        	</select>
+                        	MM <select>
+                        		<option>janvier</option>
+                        		<option>fevrier</option>
+                        		<option>mars</option>
+                        		<option>avril</option>
+                        		<option>mai</option>
+                        		<option>juin</option>
+                        		<option>juillet</option>
+                        		<option>aout</option>
+                        		<option>octobre</option>
+                        		<option>novembre</option>
+                        		<option>decembre</option>
+                        	</select>
+                        	AA <select>
+                        		<?php for($i = 1900; $i <= date('Y'); $i++) {?>
+                        		<option><?=$i ?></option>
+                        		<?php } ?>
+                        	</select>
+                        </form>
+                        
                     </div>
 					<div class="clearfix"></div>
 				</div>
-			</div>
-			<div class="admin-bottom">
-				<div class="col-md-3 admin-bottom-left">
-					<h4>Student's Area</h4>
-					<ul>
-						<li><a href="#">Student Profile</a></li>
-						<li><a href="#">Time Table</a></li>
-						<li><a href="#">Forms Download</a></li>
-						<li><a href="#">Extra Class Form</a></li>
-						<li><a href="#">Exam Seating Plan</a></li>
-					</ul>
-				</div>
-				<div class="col-md-3 admin-bottom-left">
-					<h4>Departments</h4>
-					<ul>
-						<li><a href="#">Computer Engineering</a></li>
-						<li><a href="#">Soft Engineering</a></li>
-						<li><a href="#">Management Science</a></li>
-						<li><a href="#">Earth & Environmental Science</a></li>
-						<li><a href="#">Computer Science</a></li>
-						<li><a href="#">Information Technology</a></li>
-						<li><a href="#">Electrical Engineering</a></li>
-					</ul>
-				</div>
-				<div class="col-md-3 admin-bottom-left">
-					<h4>Faculty Services</h4>
-					<ul>
-						<li><a href="#">Attendance System</a></li>
-						<li><a href="#">Employee Profile</a></li>
-						<li><a href="#">Employee Profile-Visiting Faculty</a></li>
-					</ul>
-				</div>
-				<div class="col-md-3 admin-bottom-left">
-					<h4>e-services</h4>
-					<ul>
-						<li><a href="#">e-classes</a></li>
-						<li><a href="#">e-admission</a></li>
-						<li><a href="#">e-alumni</a></li>
-						<li><a href="#">e-mail</a></li>
-						<li><a href="#">e-library</a></li>
-					</ul>
-				</div>
-				<div class="clearfix"></div>
 			</div>
 		</div>
 	</div>
